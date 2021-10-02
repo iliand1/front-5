374a9dbb3b3ed3c9a737de9084df365a5c04738e
iliand <iliandaviaud1@gmail.com>
2021-10-02 14:59:43 +0300

    d

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector(".button-left").addEventListener("click", foo);
+document.querySelector(".button-right").addEventListener("click", bar);
+function foo(params) {
+  console.log("gay left");
+}
+function bar(params) {
+  console.log("gay right");
+}
